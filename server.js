8d9e3343637b1f58b3d681df5503134638d3cc04
Vishalbarde20 <vishal12barde@gmail.com>
2024-06-13 12:40:14 +0530

    changes

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ mongoose.connect(dbConfig, { useNewUrlParser: true, useUnifiedTopology: true })
     });
 
 // Start the Server
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 10000;
 app.listen(port, '0.0.0.0', (err) => {
     if (err) {
         console.error('Failed to start server:', err);
